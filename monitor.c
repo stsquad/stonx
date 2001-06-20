c3f15cb7f058e8436c050ed3aaedbca4b4f754f4
trent <trent>
2001-06-20 16:58:44 +0000

    Re-wrote. Only a few functions at the moment.

--- monitor.c
+++ monitor.c
@@ -6,52 +6,59 @@
 #include "defs.h"
 #include "tosdefs.h"
 #include "mem.h"
-#if MONITOR
+//#if MONITOR
 #include <stdio.h>
 #include <ctype.h>
-#include <curses.h>
-#include "dis-asm.h"
+//#include <curses.h>
+//#include "dis-asm.h"
 
 #define WATCH 1
 B *smem = 0;
 W *spc	= 0;
-extern wprintw(), printw();
-extern WINDOW *stdscr;
-static disassemble_info info;
+//extern wprintw(), printw();
+//extern WINDOW *stdscr;
+//static disassemble_info info;
 static UL off = 0x10000;
 static UL count = 0;
 static UL bkpt = 0;
 static int mode=0;
 int in_monitor=0;
 
-void printcc (int c, int t)
-{
-	if (t) attron (A_REVERSE);
-	printw ("%c",c);
-	if (t) attroff (A_REVERSE);
-}
+/* disassembly routine */
+extern void* dis (char *ptr,char *string);
 
-void init_monitor (void)
+
+void init_monitor (int startflag)
 {
-	initscr();
-	cbreak();
-	keypad(stdscr,TRUE);
-	clear();
-	refresh();
-	INIT_DISASSEMBLE_INFO(info, stdscr);
-	info.fprintf_func = (fprintf_ftype)wprintw;
-	info.buffer = mem;
-	info.buffer_vma = 0;
-	info.buffer_length = MEMSIZE;
+
+  if (startflag)
+    {
+      fprintf (stderr,"Monitor being initialised (break@start).....\n");
+      in_monitor=1;
+    } else {
+      in_monitor=0;
+    }
 }
 
 void kill_monitor (void)
 {
-	endwin();
+  //endwin();
 }
 
-int update_monitor (UL *regs, int sr, int pcoff)
+/*
+** The update monitor function is called every instruction cycle
+** It should exit straight away unless
+**
+**   1. an exception has been reached (signalled by the exception flag)
+**   2. the execution count has reached 0
+**   3. the pc has hit a breakpoint address
+**   4. the in_monitor flag is set
+*/
+
+int update_monitor (UL *regs, int sr, int pcoff, int exception)
 {
+  int monitor=0;
+  int exit_val=0;
 	int i, j, x, c, cha=0, toppc, pos, lo1, eoi, yp, ddd=0;
 	static UW v=0x4e71;
 #if WATCH
@@ -60,137 +67,203 @@ int update_monitor (UL *regs, int sr, int pcoff)
 	static UL lv=0;
 	char cmd[100];
 	UL u,s;
-	if (count-- > 0 && TRIM(pcoff) != TRIM(bkpt)
-#if WATCH
-	&& LM_UB(watch) == wv
-#endif
-#if 0
-	|| (mode == 1 && SUPERVISOR_MODE)
-#endif
-	) return;
+
+	if (!in_monitor)
+	  {
+	    if (count-- > 0 && TRIM(pcoff) != TRIM(bkpt) && (!exception)
+               #if WATCH
+	       && LM_UB(watch) == wv
+               #endif
+	    ) return (exit_val);
+	  } /* endif !in_monitor */ 
+
+	
 #if WATCH
 	wv = LM_UB(watch);
 #endif
+
+	fprintf (stderr,"Entering Monitor Code\n");
+
 	in_monitor=1;
+	monitor=1;
 	toppc = pcoff;
 	count = yp = 0;
-loop:
+
 	pos = toppc;
-	for (i=0; i<8; i++)
+	dump_registers(regs,sr);
+	dissassemble (pos,10);
+
+	while (monitor)
+	  {
+	    fprintf(stdout,"STMON>");
+
+	    toppc = TRIM(toppc); off = TRIM(off); 
+	    switch (c = getchar())
+	      {
+	      case 'h':
+	      case 'H':
+	      case '?':
+		{
+		  print_help();
+		  break;
+		}
+	      case 'Q':
+	      case 'q':
+		{
+		  monitor=0;
+		  exit_val=1;
+		  break;
+		}
+	      case 'S':
+	      case 's':
+		{
+		  count=0;
+		  monitor=0;
+		  break;
+		}
+	      case 'g':
+	      case 'G':
+		{
+		  count=100;
+		  monitor=0;
+		  in_monitor=0;
+		  break;
+		}
+
+	      default:
+		break;
+	      } /* End Switch */
+
+	  } /* End While */
+	return (exit_val);
+}
+
+/* 
+** Dump Help for Debugger
+**
+*/
+
+static void print_help (void)
+{
+ 
+  fprintf (stdout,"s - step (one instruction)\n");
+  fprintf (stdout,"g - go (continue running)\n");
+  fprintf (stdout,"q - quit (leave STonX)\n");
+}
+
+/*
+** Dump n bytes in a nice word format (with a newline)
+*/
+
+static dump_bytes (int ptr, int count)
+{
+  int i;
+
+  for (i=0;i<(count/2);i++)
+    { 
+      fprintf (stdout,"%04x ", LM_UW(MEM(ptr+i)));
+    }
+  fprintf (stdout,"\n");
+}
+
+/*
+** Dump Registers D0-D7/A0-A7
+**
+** TODO:
+**  1. Proper decode of the SSR
+*/
+
+static void dump_registers (UL *regs, int sr)
+{
+  int i,j;
+  UL u,s;
+
+  fprintf (stdout,"Dumping Registers...\n");
+
+	for (i=0; i<8; i=i+4)
 	{
-		move (i,0); printw ("D%d %04x %04x", i, regs[i]>>16, regs[i]&0xffff);
+	      fprintf (stdout,"D%d: %04x %04x ", i, regs[i]>>16, regs[i]&0xffff);
+	      fprintf (stdout,"D%d: %04x %04x ", i+1, regs[i+1]>>16, regs[i+1]&0xffff);
+	      fprintf (stdout,"D%d: %04x %04x ", i+2, regs[i+2]>>16, regs[i+2]&0xffff);
+	      fprintf (stdout,"D%d: %04x %04x ", i+3, regs[i+3]>>16, regs[i+3]&0xffff);
+	      fprintf (stdout,"\n");
 	}
-	for (i=8; i<16; i++)
+	for (i=0; i<8; i=i+4)
 	{
-		move (i,0); printw ("A%d %04x %04x", i-8, regs[i]>>16, regs[i]&0xffff);
+	  fprintf (stdout,"A%d: %04x %04x ", i, regs[i+8]>>16, regs[i+8]&0xffff);
+	  fprintf (stdout,"A%d: %04x %04x ", i+1, regs[i+9]>>16, regs[i+9]&0xffff);
+	  fprintf (stdout,"A%d: %04x %04x ", i+2, regs[i+10]>>16, regs[i+10]&0xffff);
+	  fprintf (stdout,"A%d: %04x %04x ", i+3, regs[i+11]>>16, regs[i+11]&0xffff);
+	  fprintf (stdout,"\n");
 	}
 	if (sr & 0x2000)
 	{
+	  fprintf (stdout, "Supervisor Mode:");
 		u = regs[16]; s = regs[15];
 	}
 	else
 	{
+	  fprintf (stdout,"User Mode:");
 		u = regs[15]; s = regs[16];
 	}
-	move(16,0); printw ("US %04x %04x", u>>16, u&0xffff);
-	move(17,0); printw ("SS %04x %04x", s>>16, s&0xffff);
-	move(18,0); printw ("PC %08x", pcoff);
-	move(18,14); if (SUPERVISOR_MODE) printw("+"); printw ("SR %04x", sr);
-	move(18,23); printw ("CCR ");
-	printcc('X',sr & 0x10);
-	printcc('N',sr & 8);
-	printcc('Z',sr & 4);
-	printcc('V',sr & 2);
-	printcc('C',sr & 1);
-	for (i=0; i<15; i++)
-	{
-		move(i,14); printw ("%06x ",off+i*16);
-		for (j=0; j<8; j++)
-		{
-			printw ("%04x ", LM_UW(MEM(off+i*16+j*2)));
-		}
-		for (j=0; j<16; j++)
-		{
-			int o = LM_UB(MEM(off+i*16+j));
-			printw ("%c", isprint(o) ? o : '.');
-		}
-	}
-	move(16,14);
-	for (j=0; j<13; j++)
-	{
-		printw ("%04x ", LM_UW(MEM(u+j*2)));
-	}
-	move(17,14);
-	for (j=0; j<13; j++)
-	{
-		printw ("%04x ", LM_UW(MEM(s+j*2)));
-	}
-	move(19,0); clrtobot();
-	for (i=0; i<(LINES-20); i++)
-	{
-		int k;
-		if (pos == pcoff) attron(A_UNDERLINE);
-		if (pos == bkpt) attron(A_BOLD);
-		move (i+19,36);
-		k = print_insn_m68k(TRIM(pos), &info);
-		if (i == 0) lo1 = k;
-		move (i+19,0);
-		printw("[%06x] ", TRIM(pos));
-		for (j=0; j<(k/2); j++)
-			printw("%04x ", LM_UW(MEM(pos+2*j)));
-		if (pos == pcoff) attroff(A_UNDERLINE);
-		if (pos == bkpt) attroff(A_BOLD);
-		pos += k;
-	}
-	move(LINES-1,0); printw(">");
-	refresh();
-	cha = 1;
-loop1:
-	toppc = TRIM(toppc); off = TRIM(off); 
-	switch (c = getch())
-	{
-		case 'm':	getstr(cmd); off = strtol(cmd, NULL, 16) & -2; goto loop;
-		case 'p':	getstr(cmd); toppc = strtol(cmd, NULL, 16) & -2; goto loop;
-		case '=': 	getstr(cmd); pcoff=toppc=pc=TRIM(strtol(cmd,NULL,16) & -2);
-					goto loop;
-		case 's':
-		{
-			getstr(cmd); 
-			if (*cmd!=0) v = (UW)strtol(cmd,NULL,16);
-			while (LM_UW(MEM(toppc)) != v) toppc+=2;
-			goto loop;
-		}			
-		case 'S':
-		{
-			getstr(cmd); 
-			if (*cmd!=0) lv = (UL)strtol(cmd,NULL,16);
-			while (LM_UL(MEM(toppc)) != lv) toppc+=2;
-			goto loop;
-		}			
-#if WATCH
-		case 'w':	getstr(cmd); watch=MEM(strtol(cmd,NULL,16)); goto loop;
-#endif
-		case 'T':	mode ^= 1; goto loop;
-		case 'r':	clear(); goto loop;
-		case 'd':	off += 15 * 16; goto loop;
-		case 'D':	off -= 15 * 16; goto loop;
-		case 'z':	count = 10; return;
-		case 'Z': 	count = 100000; in_monitor=0; return;
-		case 'x':	count = 100; return;
-		case 'X':	count = 1000000; in_monitor=0; return;
-		case 'c':	count = 1000; return;
-		case 'v':	count = 10000; return;
-		case 'g':	count = 0x7ffffff; in_monitor=0; return;
-		case '@':	sr |= 0x700; goto loop; 
-		case 'n':	bkpt = TRIM(toppc+lo1); return;
-		case 'b':   getstr(cmd); bkpt = strtol(cmd, NULL, 16) & -2; goto loop;
-		case KEY_DOWN: toppc += lo1; goto loop;
-		case KEY_UP: toppc -= 2; goto loop;
-		case '>': toppc = pos; goto loop;
-		case '<': toppc -= 2*(LINES-20); goto loop;
-	}
-	return c;
+	fprintf (stdout, "%04x:", u);
+	if (u&0x01) fprintf (stdout,"C");
+	if (u&0x02) fprintf (stdout,"O");
+	if (u&0x04) fprintf (stdout,"Z");
+	if (u&0x08) fprintf (stdout,"N");
+	if (u&0x10) fprintf (stdout,"X");
+
+	fprintf (stdout,"\n");
 }
 
-#endif /* MONITOR */
+/*
+** AJB: I don't know where this came from but for now it thunks to
+** the new 68k dissassembly routine.. not neat but should work. As
+** Jims 68k dissassembly thinks its on a 68k I need to copy from
+** the 68k core to a buffer scanned by dis();
+**
+** TODO:
+*/
+
+static int dissassemble (int pos, int count)
+{
+  int i,j,k;
+  int old_pos;
+  char mem_string[10];
+  char *local_mem;
+  char *string,*string2;
+
+  string=malloc(80);
+  if (!string)
+    { 
+      free(string); 
+      return; 
+    }
+
+
+  for (i=0; i<count; i++)
+    {
+      old_pos=pos;
+      pos = dis (pos,string);
+      k = pos-old_pos;
+      fprintf(stdout,"%-50s",string);
+
+      fprintf(stdout,"[");
+     for (j=0; j<k; j=j+2)
+      {
+	fprintf(stdout,"%04x ", LM_UW(MEM(old_pos+j)));
+      }
+
+      fprintf(stdout,"]\n");
+   }
+
+  // Free memory!
+  free(string);
+
+  return;
+	
+}
+
+
+//#endif /* MONITOR */
 
