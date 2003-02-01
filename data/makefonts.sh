9ad3ab8705eab8c5a263aa8bd5cfb06066224ea9
thothy <thothy>
2003-02-01 09:59:59 +0000

    Fixed font generation script

--- data/makefonts.sh
+++ data/makefonts.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=${PATH}:..
+
 if [ ! -f tos.img ]; then
 	echo "Please copy a TOS image to this directory as tos.img"
 	exit 1
@@ -7,18 +9,13 @@ fi
 
 echo "Making the System fonts..."
 
-test -d data || mkdir data
 tosfonts < tos.img
-mv System?.fnt data
-
-cd data
 
 for i in *.fnt; do
 fnttobdf <$i | bdftopcf > `basename $i .fnt`.pcf
 fnttobdf -tr <$i | bdftopcf > `basename $i .fnt`-iso.pcf
 done
+
 mkfontdir .
 xset fp+ `pwd`
 xset fp rehash
-
-cd ..
